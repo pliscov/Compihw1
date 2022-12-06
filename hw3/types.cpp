db30449068f2b923d6271a3a3aa78363a5a51604
pliscov <64476145+pliscov@users.noreply.github.com>
2022-12-06 06:22:16 +0200

    some progress in everything
    started scoping, didn't get to:
    if, else, while scopes
    did get to function scope
    did do some of the checks including:
    function call parameters,
    some ID declaration checks
    return types mismatch
    
    also implemented most of the types.cpp functions and changed some stuff in these structures.
    
    i ran out of sleep at 02:00 and decided to work on this (in a disorganized manner) until 06:00 or so lmao

--- hw3/types.cpp
+++ hw3/types.cpp
@@ -1,1 +1,102 @@
-1
+#include "types.hpp"
+#include <vector>
+
+// YYSTYPE //
+
+// FUNC //
+
+Func::Func(std::vector<std::string> types, std::string ret_type){
+    this->types = std::vector<std::string>(types);
+    this->ret_type = ret_type;
+}
+
+bool Func::legalRet(std::string ret_type){
+    return this->ret_type == ret_type;
+}
+
+bool Func::legalParams(std::vector<std::string> param_list){
+    if (this->types.size() != param_list.size())
+        return false;
+    for (int i = 0; i < types.size(); i++)
+        if (param_list[i] != types[i])
+            return false;
+    return true;
+}
+
+// SYMBOL TABLE //
+
+SymbolTable::SymbolTable(std::string scope_type){
+    this->scope_type = scope_type;
+    this->table = new std::vector<YYSTYPE>();
+}
+
+SymbolTable::SymbolTable(std::string scope_type, std::string ret_val){
+    this->scope_type = scope_type;
+    this->table = new std::vector<YYSTYPE>();
+    this->ret_val = ret_val;
+}
+
+YYSTYPE* SymbolTable::get(std::string symbol_name){
+    for (YYSTYPE y: table)
+        if (y.name == symbol.name)
+            return *y;
+    return NULL;
+}
+
+// TABLE MANAGER //
+TableManager::TableManager(){
+    offset_stack = new std::vector<int>();
+    tables_stack = new std::vector<SymbolTable>();
+    newScope("global");
+    // init print and printi
+    Func& print(std::vector<std::string>("string"), std::string("void"));
+    Func& printi(std::vector<std::string>("int"), std::string("void"));
+    insert(print);
+    insert(printi);
+    offset_stack.push_back(0);
+}
+
+
+/*
+* this method is for inserting a new function parameter into the top symbol table (current scope)
+*/
+void TableManager::insert(YYSTYPE& var){
+    tables_stack.back().push_back(var);
+}
+
+/*
+* this method is for inserting a new var with an offset to the top symbol table
+*/
+void TableManager::insertID(YYSTYPE& id){
+    offset_stack.back() += 1;
+    tables_stack.back().push_back(id);
+}
+
+void TableManager::newScope(std::string scope_type){
+    tables_stack.push_back(new SymbolTable(scope_type));
+    if (!offset_stack.empty())
+        offset_stack.push_back(offset_stack.back());
+}
+
+void TableManager::popScope(){
+    tables_stack.pop_back();
+}
+
+
+YYSTYPE* TableManager::get(std::string symbol_name){
+    YYSTYPE* y;
+    for (std::iterator<SymbolTable> i = tables_stack.end(); i >= tables_stack.begin(); --i;){
+        if (y = i->get(symbol_name) != NULL){
+            return *y; 
+        }
+    }
+    return NULL;
+}
+
+
+
+
+
+
+
+
--- hw3/types.hpp
+++ hw3/types.hpp
@@ -3,72 +3,80 @@
 #include <vector>
 
 class YYSTYPE {
-    protected:
-        const string value;
+    public:
+        std::vector<std::string> values;
         const string name;
         const string type;
         int offset;
-        
-    public:
-        std::string get_ret_val();
-        YYSTYPE(string value, string name, string type):
-         value(value), name(name), type(type){}
+        YYSTYPE() = default;
+        YYSTYPE(string name, string type):
+         name(name), type(type){}
+        YYSTYPE(string values, string name, string type):
+         values(values), name(name), type(type){}
+        YYSTYPE(string name, string type, int offset):
+         name(name), type(type), offset(offset){}
+
         
 }
 
-class NUM : public YYSTYPE{}
-
 class Func: public YYSTYPE{
-    private:
-        std::vector<std::string> params;
-        std::string ret_val;
-
     public:
-        std::vector<std::string> getParams();
+        std::vector<std::string> types;
+        std::string ret_type;
+
+        Func(std::vector<std::string> types, std::string ret_type);
         bool legalRet(std::string ret_type);
+        bool legalParams(std::string param_list);
+}
+
+class FormalsList{
+    public:
+        std::vector<YYSTYPE> list;
 }
 
 class SymbolTable{
     private:
         std::vector<YYSTYPE> table;
-        std::string scope_type;
 
     public:
-        YYSTYPE get(std::string symbol_name);
+        const std::string ret_val;
+        const std::string scope_type;
+        SymbolTable(std::string scope_type);
+        SymbolTable(std::string scope_type, std::string ret_val);
+        YYSTYPE* get(std::string symbol_name);
         void push();
 
 
 }
 
-class TableStack{
-    private:
-        std::vector<SymbolTable> s;
-        void Maketable();
-    public:
-        void insert(YYSTYPE symbol);
-        void push(std::string scope_type);
-        void pop();
-        YYSTYPE get(std::string symbol_name);
-        SymbolTable top();
+// class TableStack{
+//     private:
+//         std::vector<SymbolTable> s;
+//         void Maketable();
+//     public:
+//         void insert(YYSTYPE symbol);
+//         void push(std::string scope_type);
+//         void pop();
+//         YYSTYPE get(std::string symbol_name);
+//         SymbolTable top();
 
 
-}
+// }
 
 class TableManager{
     private:
-        TableStack table_stack;
+        std::vector<SymbolTable> tables_stack;
         std::vector<int> offset_stack;
     public:
         TableManager(); //print and printi
-        void insert();
+        void insert(YYSTYPE& var);
+        void insertID(YYSTYPE& var);
         void newScope(std::string scope_type);
         void popScope();
         bool contains(std::string type);
         bool inScope(std::string scope_type);
-        bool legalReturn(std::string return_type)
-        YYSTYPE get(std::string symbol_name);
+        //bool legalReturn(std::string return_type)     instead of this i put a ret_type on a scope as an optional field -alon
+        YYSTYPE* get(std::string symbol_name);
 }
-
-
 
 
